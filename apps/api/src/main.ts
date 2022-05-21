5e34248fc38ff21d3f033a08d9a373717ee6c92f
Matthew McCall <46231996+mxtt-mmxix@users.noreply.github.com>
2022-05-21 19:49:19 -0400

    Added Docker support

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,13 +9,24 @@ const io = new Server(server);
 
 const greeting: Message = { message: 'Welcome to api!' };
 
+app.use(express.static('./dist/apps/client', {
+    index: false
+}))
+
 app.get('/api', (req, res) => {
-  res.send(greeting);
+    res.send(greeting);
 });
+
+app.get(/(.*)/, (req, res) => {
+    res.sendFile('index.html', {
+        root: './dist/apps/client'
+    })
+})
 
 const port = process.env.port || 3333;
 
 app.listen(port, () => {
-  console.log('Listening at http://localhost:' + port + '/api');
+    console.log('Listening at http://localhost:' + port + '/api');
 });
+
 server.on('error', console.error);
